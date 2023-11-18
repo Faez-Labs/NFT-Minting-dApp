ab540d853b04ca07a7115ea39bd50877ff0543de
Faysal <faysalbadaoui02@gmail.com>
2023-11-18 13:18:32 +0100

    deploy script working and basic nft minting sc

--- scripts/run.js
+++ scripts/run.js
@@ -1,12 +1,21 @@
 async function main() {
 
   const lock = await hre.ethers.deployContract("MySmartNFT");
-
   await lock.waitForDeployment();
-
   console.log(
     `Deployed to ${lock.target}`
   );
+  // Call the function.
+  let txn = await nftContract.makeAnEpicNFT()
+  // Wait for it to be mined.
+  await txn.wait()
+
+  // Mint another NFT for fun.
+  txn = await nftContract.makeAnEpicNFT()
+  // Wait for it to be mined.
+  await txn.wait()
+
+
 }
 
 main().catch((error) => {
