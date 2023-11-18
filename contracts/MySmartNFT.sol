6ce814af5e2e331c593a038b2ea32b6d2645dcfe
Faysal <faysalbadaoui02@gmail.com>
2023-11-18 12:28:38 +0100

    fix error to deloy smart contract

--- contracts/MySmartNFT.sol
+++ contracts/MySmartNFT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 
 import "hardhat/console.sol";
 
-contract MyEpicNFT {
+contract MySmartNFT {
     constructor() {
         console.log("This is my NFT contract. Whoa!");
     }
