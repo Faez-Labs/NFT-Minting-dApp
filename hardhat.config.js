37a6b2586e2f9ab2c316837fae8fdebf973b5687
Faysal <faysalbadaoui02@gmail.com>
2023-11-18 13:53:28 +0100

    added goerlii as testNet for hardhat fdeploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
@@ -14,5 +15,11 @@ module.exports = {
         version: "0.8.20"
       }
     ]
-  }
+  },
+  networks: {
+    goerli: {
+      url: process.env.QUICKNODE_API_KEY_URL,
+      accounts: [process.env.GOERLI_PRIVATE_KEY],
+    },
+  },
 };
