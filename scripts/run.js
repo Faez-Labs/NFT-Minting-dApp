eebbf284dc129b3bb482f5131582fe6fe1ba5252
Faysal <faysalbadaoui02@gmail.com>
2023-11-18 12:27:47 +0100

    changed solidify version fr hardhat

--- scripts/run.js
+++ scripts/run.js
@@ -1,5 +1,5 @@
 const main = async () => {
-    const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
+    const nftContractFactory = await hre.ethers.getContractFactory('MySmartNFT');
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
